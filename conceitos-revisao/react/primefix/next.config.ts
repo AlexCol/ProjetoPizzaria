c7a5054dcb9cb801861e4b29eaaaf2a37ba2f908
AlexCol <eu_axil@yahoo.com.br>
2025-07-16 09:20:20 -0300

    .

--- conceitos-revisao/react/primefix/next.config.ts
+++ conceitos-revisao/react/primefix/next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
-  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
   //reactStrictMode: false
 };
 
