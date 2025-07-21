ee06ba5f3903dff49ee9015e3c1c809551f77a51
AlexCol <eu_axil@yahoo.com.br>
2025-07-21 20:59:55 -0300

    preparando processo de autenticação

---
+++ projeto/backend/nest/src/modules/auth/auth.module.ts
@@ -0,0 +1,28 @@
+import { Global, Module } from "@nestjs/common";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
+import { APP_GUARD } from "@nestjs/core";
+import { ConfigModule } from "@nestjs/config";
+import jwtConfig from "./config/jwt.config";
+import { AuthTokenGuard } from "./guards/authToken.guard";
+import { DomainModule } from "../domain/domain.module";
+import { IHashingService } from "./hashing/hashing.service";
+import { BcryptService } from "./hashing/bcrypt.service";
+import { JwtModule } from "@nestjs/jwt";
+
+@Global()
+@Module({
+  imports: [
+    ConfigModule.forFeature(jwtConfig), // Importing the ConfigModule for JWT configuration
+    JwtModule.registerAsync(jwtConfig.asProvider()), // Registering the JwtModule with the JWT configuration
+    DomainModule
+  ],
+  controllers: [AuthController],
+  providers: [
+    AuthService,
+    { provide: IHashingService, useClass: BcryptService }, // Providing the IHashingService interface with the BcryptService implementation
+    { provide: APP_GUARD, useClass: AuthTokenGuard }
+  ],
+  exports: []
+})
+export class AuthModule { }
