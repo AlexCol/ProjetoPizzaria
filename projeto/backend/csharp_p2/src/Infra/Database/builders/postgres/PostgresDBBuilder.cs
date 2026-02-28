b4cbfa5455ebef22a36f88481b2bef91720339dc
AlexCol <eu_axil@yahoo.com.br>
2026-02-28 09:50:13 -0300

    ajustes conexão postgres

--- projeto/backend/csharp_p2/src/Infra/Database/builders/postgres/PostgresDBBuilder.cs
+++ projeto/backend/csharp_p2/src/Infra/Database/builders/postgres/PostgresDBBuilder.cs
@@ -1,13 +1,26 @@
 using csharp_p2.src.Config;
 using csharp_p2.src.Infra.Database.builders;
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 namespace csharp_p2.src.Infra.Database;
 
 public static partial class DataBaseBuilder {
   private static void AddPostgres(this WebApplicationBuilder builder, EnvConfig env) {
     var database = env.Database;
-    var connectionString = $"Host={database.Host};Port={database.Port};Database={database.Name};Username={database.User};Password={database.Password}";
+    var connectionStringBuilder = new NpgsqlConnectionStringBuilder {
+      Host = database.Host,
+      Port = int.Parse(database.Port),
+      Database = database.Name,
+      Username = database.User,
+      Password = database.Password,
+      MinPoolSize = database.MinPool,
+      MaxPoolSize = database.MaxPool,
+      Timeout = Math.Max(1, database.AcquireTimeoutMillis / 1000),
+      ConnectionIdleLifetime = Math.Max(1, database.IdleTimeoutMillis / 1000)
+    };
+
+    var connectionString = connectionStringBuilder.ConnectionString;
     builder.Services.AddDbContext<PostgresDBContext>(options =>
         options.UseNpgsql(connectionString));
     builder.Services.AddScoped<BaseDBContext>(sp => sp.GetRequiredService<PostgresDBContext>());
