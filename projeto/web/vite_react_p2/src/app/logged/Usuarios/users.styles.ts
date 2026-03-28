9e84cfee280614cc59a4873dbd42a70650ef6733
AlexCol <eu_axil@yahoo.com.br>
2026-03-27 22:25:59 -0300

    .

--- projeto/web/vite_react_p2/src/app/logged/Usuarios/users.styles.ts
+++ projeto/web/vite_react_p2/src/app/logged/Usuarios/users.styles.ts
@@ -1,5 +1,5 @@
 const usersStyles = {
-  containerTC: `w-full h-full bg-background overflow-auto sidebar-scrollbar-personalizado`,
+  containerTC: `w-full h-full overflow-auto sidebar-scrollbar-personalizado`,
   innerContainerTC: `h-full flex flex-col mx-7`,
 };
 export default usersStyles;
--- projeto/web/vite_react_p2/src/components/layout/Header/header.styles.tsx
+++ projeto/web/vite_react_p2/src/components/layout/Header/header.styles.tsx
@@ -1,5 +1,5 @@
 export const headerStyles = {
-  header: `h-14 flex items-center justify-between px-4 border-b border-border`,
+  header: `h-14 flex items-center justify-between px-4 border-b border-border transition-colors duration-300`,
   menuItens: `flex-1 h-full w-full flex justify-end items-center`,
   logoLink: ``,
   logoWrapTC: `w-8 h-8`,
